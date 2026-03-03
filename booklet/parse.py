accfb12769c52db3292cddf369d1217920e6d446
Stephan Balthasar <stephan.balthasar@uni-bayreuth.de>
2026-03-03 10:25:05 +0100

    Create parse.py

---
+++ booklet/parse.py
@@ -0,0 +1,7 @@
+def parse_booklet(docx_path: str):
+    """
+    Returns:
+        chunks: List[str]
+        metas:  List[dict] (e.g., {'paras': [...], 'cases': [...], 'page_num': int})
+    """
+    raise NotImplementedError
